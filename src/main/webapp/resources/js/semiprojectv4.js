a4476ba5502dde7e5db15dc70dbe6cdb226721f6
hideonbush3 <ppoii0961@gmail.com>
2023-05-01 15:44:15 +0900

    회원가입 이용약관 페이지

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -1,20 +1,35 @@
+// header 로고 클릭 이벤트
 let logo = document.querySelector('#logo');
-let agreeok = document.querySelector('#agreeok');
-let check2btn = document.querySelector('#check2btn');
-let joinbtn = document.querySelector('#joinbtn');
-
 logo.addEventListener('click', function (){
     location.href = '/';
 })
 
-agreeok.addEventListener('click', function (){
-    location.href = '/join/checkme';
+// 이용약관 체크박스
+let agree1 = document.querySelector('#agree1');
+
+// 개인정보 이용 체크박스
+let agree2 = document.querySelector('#agree2');
+
+// 동의 버튼
+let agreeok = document.querySelector('#agreeok');
+agreeok?.addEventListener('click', () => {
+    if (!agree1.checked) alert("이용약관 동의에 체크하세요");
+    else if(!agree2.checked) alert("개인정보 이용 동의에 체크하세요");
+    else location.href = '/join/checkme';
 })
 
-check2btn.addEventListener('click', function (){
+// 비동의 버튼
+let noagreeok = document.querySelector('#noagreeok');
+noagreeok?.addEventListener('click', () =>{
+    location.href = '/';
+})
+
+
+
+check2btn?.addEventListener('click', () => {
     location.href = '/join/joinme';
 })
 
-joinbtn.addEventListener('click', function (){
+joinbtn?.addEventListener('click', () => {
     location.href = '/join/checkok'
 })
