8c9ee82cc9ae68d491d024f91970e77d07db878e
hideonbush3 <ppoii0961@gmail.com>
2023-05-02 11:16:10 +0900

    회원가입 - 우편번호검색 RESTful 2 - service

---
+++ src/main/java/hideonbush3/spring4/mvc/semiprojectv4/service/JoinService.java
@@ -0,0 +1,13 @@
+package hideonbush3.spring4.mvc.semiprojectv4.service;
+
+import hideonbush3.spring4.mvc.semiprojectv4.model.Member;
+
+public interface JoinService {
+    String findZipcode(String dong);
+
+    boolean newMember(Member m);
+
+    boolean checkUserid(String uid);
+
+    boolean loginMember(Member m);
+}
