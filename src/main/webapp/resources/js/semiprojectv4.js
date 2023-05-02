ccde1daec23e3d28b4a8472cf7a41dabfaf91297
hideonbush3 <ppoii0961@gmail.com>
2023-05-02 12:50:33 +0900

    회원가입 - 우편번호검색 RESTful 5 - ajax fetch zipcode

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -49,22 +49,26 @@ cancel2btn?.addEventListener('click', () => {
 const joinfrm = document.forms.joinfrm;
 const joinbtn = document.querySelector('#joinbtn');
 const cancelbtn = document.querySelector('#cancelbtn');
-const sendzip = document.querySelector('#sendzip');
 
-sendzip?.addEventListener('click', ()=>{
-    joinfrm.zip1.value = addrlist.substring(0, 3);
-    joinfrm.zip2.value = addrlist.substring(4, 7);
-})
+// 우편번호 검색 모달
+const dong = document.querySelector('#dong');           // 동 입력칸
+const zipbtn = document.querySelector('#findzipbtn');   // 우편번호 검색 버튼
+const addrlist = document.querySelector('#addrlist');   // 검색결과 표시하는 option 태그
+const sendzip = document.querySelector('#sendzip');     // 우편번호 선택하고 닫는 버튼
+const zip1 = document.querySelector('#zip1');       // 우편번호 앞자리 입력칸
+const zip2 = document.querySelector('#zip2');       // 우편번호 뒷자리 입력칸
+
+
 joinbtn?.addEventListener('click', () => {
     if (joinfrm.userid.value === '') alert("아이디를 입력하세요")
     else if(joinfrm.passwd.value === '') alert("비밀번호를 입력하세요");
     else if(joinfrm.repasswd.value === '') alert("비밀번호를 한번 더 입력하세요");
     else if(joinfrm.passwd.value !== joinfrm.repasswd.value) alert("비밀번호가 일치하지 않습니다");
-    else if(joinfrm.zip1.value === '' || joinfrm.zip2.value === '') alert("우편번호를 확인하세요");
+    // else if(joinfrm.zip1.value === '' || joinfrm.zip2.value === '') alert("우편번호를 확인하세요");
     else if(joinfrm.addr1.value === '' || joinfrm.addr2.value === '') alert("주소를 입력하세요");
-    else if(joinfrm.email1.value === '' || joinfrm.email2.value === '') alert("이메일을 입력하세요");
+    // else if(joinfrm.email1.value === '' || joinfrm.email2.value === '') alert("이메일을 입력하세요");
     else if(joinfrm.tel2.value === '' || joinfrm.tel3.value === '') alert("전화번호를 입력하세요");
-    else if(joinfrm.grecaptcha.value === '') alert("자동가입방지를 확인하세요");
+    // else if(joinfrm.grecaptcha.value === '') alert("자동가입방지를 확인하세요");
     else {
         location.href = "/join/joinok";
     }
@@ -73,6 +77,39 @@ joinbtn?.addEventListener('click', () => {
 cancelbtn?.addEventListener('click', () => {
     location.href = "/join/checkme";
 })
+
+const showzipaddr = (jsons) => {
+    // for(idx in jsons){
+    //     console.log(jsons[idx]);
+    // }
+
+    // 4. json 문자열을 객체로 변환
+    jsons = JSON.parse(jsons);
+    let addrs = '';
+    // console.log("제이슨 문자열을 객체로 바꿈!!!!!    " + jsons + "     제이슨 문자열을 객체로 바꿈!!!!!    ")
+    // jsons.forEach(function (data, idx) {
+    //     console.log(data);
+    // });
+    jsons.forEach(function (data, idx) {
+        // console.log(`${data['zipcode']} ${data['sido']} ${data['gugun']} ${data['dong']}`);
+        addrs += `<option>${data['zipcode']} ${data['sido']} ${data['gugun']} ${data['dong']}</option>`;
+    });
+    while(addrlist.lastChild){
+        addrlist.removeChild(addrlist.lastChild);
+    }
+    addrlist.innerHTML = addrs;
+};
+
+// 1. fetch() 함수로 url 요청
+// 2. joinController 에서 json 문자열 형식으로 결과를 보내줌
+// 3. 그것을 인자로 showzipaddr() 호출. showzipaddr()은 우편번호 검색 결과를 보여주는 함수
+zipbtn?.addEventListener('click', () => {
+    const url = '/join/zipcode?dong=' + dong.value;
+    fetch(url).then(response => response.text())
+        .then(text => showzipaddr(text));
+});
+
+
 
 
 
