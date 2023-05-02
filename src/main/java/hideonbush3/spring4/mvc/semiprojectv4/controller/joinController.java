7ebbb0c38f063f15034f20ecd6a87f9df98dddbe
hideonbush3 <ppoii0961@gmail.com>
2023-05-02 17:12:49 +0900

    회원가입 - 아이디 중복체크 RESTful - controller

--- src/main/java/hideonbush3/spring4/mvc/semiprojectv4/controller/joinController.java
+++ src/main/java/hideonbush3/spring4/mvc/semiprojectv4/controller/joinController.java
@@ -56,4 +56,19 @@ public class joinController {
             e.printStackTrace();
         }
     }
+
+    // 아이디 사용가능 여부
+    // /join/checkuid?uid=아이디
+    // 사용가능     :0
+    // 사용불가능    :1
+    @ResponseBody
+    @GetMapping("/checkuid")
+    public void checkuid(String uid, HttpServletResponse res){
+        try {
+            // 아이디 사용여부를 뷰없이 바로 응답으로 출력
+            res.getWriter().print( jnsrv.checkUserid(uid) );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
