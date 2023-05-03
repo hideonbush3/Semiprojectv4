090a50e4202e41a97eefde5334586595def1cd3e
hideonbush3 <ppoii0961@gmail.com>
2023-05-03 10:53:32 +0900

    회원가입 - 입력값 체크

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -66,7 +66,7 @@ const zpmdbtn = document.querySelector('#zpmdbtn');
 
 const modal = new bootstrap.Modal(zipmodal, {});
 
-joinbtn?.addEventListener('click', () => {
+joinbtn?.addEventListener('click', (e) => {
     if (joinfrm.userid.value === '') alert("아이디를 입력하세요")
     else if(joinfrm.passwd.value === '') alert("비밀번호를 입력하세요");
     else if(joinfrm.repasswd.value === '') alert("비밀번호를 한번 더 입력하세요");
@@ -76,7 +76,11 @@ joinbtn?.addEventListener('click', () => {
     else if(joinfrm.email1.value === '' || joinfrm.email2.value === '') alert("이메일을 입력하세요");
     else if(joinfrm.tel2.value === '' || joinfrm.tel3.value === '') alert("전화번호를 입력하세요");
     else if(grecaptcha.getResponse() === '') alert("자동가입방지를 확인하세요");
+    else if(joinfrm.checkuid.value === 'no') alert("아이디 중복 체크하세요");
     else {
+        joinfrm.zipcode.value = joinfrm.zip1.value + '-' + joinfrm.zip2.value;
+        joinfrm.email.value = joinfrm.email1.value + '@' + joinfrm.email2.value;
+        joinfrm.phone.value = joinfrm.tel1.value + '-' + joinfrm.tel2.value + '-' + joinfrm.tel3.value;
         joinfrm.method = 'post';
         joinfrm.action = '/join/joinok';
         joinfrm.submit();
@@ -172,17 +176,22 @@ dong?.addEventListener('keydown', (e) => {
 const styleCheckUid = (chkuid) => {
     let msg = '사용불가능한 아이디입니다';
     uidmsg.style.color = 'red';
-    uidmsg.innerText = msg;
+    joinfrm.checkuid.value = 'no';
+
     if(chkuid === '0'){
         msg = '사용 가능한 아이디입니다';
         uidmsg.style.color = 'blue';
         uidmsg.innerText = msg;
+        joinfrm.checkuid.value = 'yes';
     }
+    uidmsg.innerText = msg;
 };
+
 userid?.addEventListener('blur', () => {
     if(userid.value === ''){
         uidmsg.innerText = '6~16 자의 영문 소문자, 숫자와 특수기호(_)만 사용할 수 있습니다.';
         uidmsg.style.color = 'grey';
+        joinfrm.checkuid.value = 'no';
         return;
     }
     const url = "/join/checkuid?uid=" + userid.value;
